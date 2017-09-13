c6385764ea6e863c8b029b8ca44b941d90527682
Jamin <paranoimin@gmail.com>
2017-09-13 14:52:09 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Mobile Namecard Main</title>
+
+    <link rel="stylesheet" type="text/css" href="./src/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="./src/css/common.css">
+    <link rel="stylesheet" type="text/css" href="./src/css/index.css">
+
+    <script type="text/javascript" src="./src/js/jquery.min.js" charset="utf-8"></script>
+    <script type="text/javascript" src="./src/js/common.js" charset="utf-8"></script>
+    <script type="text/javascript" src="./src/js/index.js" charset="utf-8"></script>
+
+</head>
+
+<body>
+    <div id="wrap">
+
+        <header id="header">
+            <h1>서비스 메인 화면</h1>
+        </header>
+
+        <section id="section">
+            <h2>모바일 명함</h2>
+        </section>
+
+        <footer id="footer">
+            <h2>푸터</h2>
+        </footer>
+
+    </div>
+</body>
+
+</html>
